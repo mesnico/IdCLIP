a5efaf9b595ca7ba6060a4255b758cf39c7727d6
mesnico <nicola.messina93@gmail.com>
2024-02-09 16:13:28 +0100

    Fixed end epoch logging and entity-based validation

--- src/model/idclip.py
+++ src/model/idclip.py
@@ -39,6 +39,7 @@ class IdCLIP(LightningModule):
         # store validation values to compute retrieval metrics
         # on the whole validation set
         self.validation_step_t_latents = []
+        self.validation_step_t_entity_latents = []
         self.validation_step_v_latents = []
         self.text_to_image_map = []
         self.image_to_text_map = []
@@ -116,47 +117,40 @@ class IdCLIP(LightningModule):
             )
         return losses["loss"]
 
-    def validation_step(self, batch: Dict, batch_idx: int, dataloader_idx: int = 0) -> Tensor:
-        if dataloader_idx != 0:
-            return
-        
+    def validation_step(self, batch: Dict, batch_idx: int, dataloader_idx: int = 0) -> Tensor:        
         bs = len(batch[0])
         t_latents, v_latents = self.forward(*batch)
         # t_latents = t_latents.cpu()
         # v_latents = v_latents.cpu()
 
         # Store the latent vectors
-        self.validation_step_t_latents.append(t_latents)
-        self.validation_step_v_latents.append(v_latents)
+        if dataloader_idx == 0:
+            # store the 5-per-image text latents carrying the tok part + original caption
+            self.validation_step_t_latents.append(t_latents)
+            self.validation_step_v_latents.append(v_latents)
+        elif dataloader_idx == 1:
+            # store only the latent for the tok (entity) part (the images are unchanged)
+            self.validation_step_t_entity_latents.append(t_latents)
 
-        # text has shape B x 5 x 77
-        batch_size, captions_per_image, _ = batch[1].shape
+        if dataloader_idx == 0:
+            # update some data structures used to compute retrieval metrics
+            # text has shape B x 5 x 77
+            batch_size, captions_per_image, _ = batch[1].shape
 
-        # Update text_to_image_map and image_to_text_map for this batch
-        for i in range(batch_size):
-            # the next image corresponds to text captions [text_index ... text_index + captions_per_image - 1]
-            text_index = (batch_idx * batch_size + i) * captions_per_image
-            text_indices = list(range(text_index, text_index + captions_per_image))
-            self.image_to_text_map.append(text_indices)
+            # Update text_to_image_map and image_to_text_map for this batch
+            for i in range(batch_size):
+                # the next image corresponds to text captions [text_index ... text_index + captions_per_image - 1]
+                text_index = (batch_idx * batch_size + i) * captions_per_image
+                text_indices = list(range(text_index, text_index + captions_per_image))
+                self.image_to_text_map.append(text_indices)
 
-            # Each of the next captions_per_image text captions correspond to the same image
-            self.text_to_image_map += [batch_idx * batch_size + i] * captions_per_image
-
-        # for loss_name in sorted(losses):
-        #     loss_val = losses[loss_name]
-        #     self.log(
-        #         f"val_{loss_name}",
-        #         loss_val,
-        #         on_epoch=True,
-        #         on_step=True,
-        #         batch_size=bs,
-        #     )
-
-        # return losses["loss"]
+                # Each of the next captions_per_image text captions correspond to the same image
+                self.text_to_image_map += [batch_idx * batch_size + i] * captions_per_image
 
     def on_validation_epoch_end(self):
         # Compute contrastive metrics on the whole batch
         t_latents = torch.cat(self.validation_step_t_latents)
+        t_entity_latents = torch.cat(self.validation_step_t_entity_latents)
         v_latents = torch.cat(self.validation_step_v_latents)
 
         text_to_image_map = torch.LongTensor(self.text_to_image_map).to(t_latents.device)
@@ -171,7 +165,7 @@ class IdCLIP(LightningModule):
         # Get the entities dataset
         val_dataset = self.trainer.val_dataloaders[1].dataset
         entities_metrics = only_tok_metrics(
-            v_latents, t_latents, val_dataset, k_vals
+            v_latents, t_entity_latents, val_dataset, k_vals
         )
 
         metrics = {**contrastive_metrics, **entities_metrics}
@@ -189,6 +183,7 @@ class IdCLIP(LightningModule):
 
         self.validation_step_t_latents.clear()
         self.validation_step_v_latents.clear()
+        self.validation_step_t_entity_latents.clear()
         self.text_to_image_map.clear()
         self.image_to_text_map.clear()
 
