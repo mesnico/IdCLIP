0f3927c040f4d536f698f7d24c126426977de5ae
mesnico <nicola.messina93@gmail.com>
2024-02-16 16:06:11 +0100

    multi-layer MLP translator

--- src/model/translators.py
+++ src/model/translators.py
@@ -2,28 +2,18 @@ from torch import nn
 from torch.nn import functional as F
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, num_hidden_layers=1, hidden_dim=768):
         super().__init__()
 
-        self.input_fc = nn.Linear(input_dim, 768, bias=False)
-        #self.hidden_fc_1 = nn.Linear(768, 768, bias=False)
-        #self.hidden_fc_2 = nn.Linear(1024, 1024, bias=False)
-        #self.hidden_fc_3 = nn.Linear(1024, 2048, bias=False)
-        #self.hidden_fc_4 = nn.Linear(2048, 1024, bias=False)
-        #####dropout
-        #self.dropout_1 = nn.Dropout(dropout)
-        #self.dropout_2 = nn.Dropout(dropout)
-        self.output_fc = nn.Linear(768, output_dim, bias=False)
-        #self.output_fc = nn.Linear(input_dim, output_dim, bias=False)
-        
+        # create a sequence of num_layers linear layers using a ModuleList
+        self.linears = nn.ModuleList([
+            nn.Linear(input_dim if i == 0 else hidden_dim, hidden_dim, bias=False) 
+            for i in range(num_hidden_layers)
+        ])
+        self.out = nn.Linear(hidden_dim, output_dim, bias=False)
 
     def forward(self, x):
-        x = F.relu(self.input_fc(x))
-        #x = self.dropout_1(x)
-        #x = F.relu(self.hidden_fc_1(x))
-        #x = self.dropout_2(x)
-        #x = F.relu(self.hidden_fc_2(x))
-        #x = F.relu(self.hidden_fc_3(x))
-        #x = F.relu(self.hidden_fc_4(x))
-        x = self.output_fc(x)
+        for layer in self.linears:
+            x = F.relu(layer(x))
+        x = self.out(x)
         return x
