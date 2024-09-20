6fcfabdac8248fda8ea57dc7d1ba36d900033a57
mesnico <nicola.messina93@gmail.com>
2024-09-20 09:48:42 +0000

    retrieval tokenizer fix, bias control for MLP

--- retrieval.py
+++ retrieval.py
@@ -21,7 +21,7 @@ def retrieval(newcfg: DictConfig) -> None:
     cfg = read_config(run_dir)
     model = instantiate(cfg.model)
     transform = model.transform
-    target_transform=lambda texts: clip.tokenize(texts)
+    target_transform=model.tokenizer
 
     # load the checkpoint
     ckpt_path = glob.glob(os.path.join(run_dir, "logs/checkpoints", f"{ckpt_name}*.ckpt"))
--- src/model/translators.py
+++ src/model/translators.py
@@ -2,15 +2,15 @@ from torch import nn
 from torch.nn import functional as F
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim, num_hidden_layers=1, hidden_dim=768):
+    def __init__(self, input_dim, output_dim, num_hidden_layers=1, hidden_dim=768, bias=False):
         super().__init__()
 
         # create a sequence of num_layers linear layers using a ModuleList
         self.linears = nn.ModuleList([
-            nn.Linear(input_dim if i == 0 else hidden_dim, hidden_dim, bias=False) 
+            nn.Linear(input_dim if i == 0 else hidden_dim, hidden_dim, bias=bias) 
             for i in range(num_hidden_layers)
         ])
-        self.out = nn.Linear(hidden_dim, output_dim, bias=False)
+        self.out = nn.Linear(hidden_dim, output_dim, bias=bias)
 
     def forward(self, x):
         for layer in self.linears:
